a23583ba43386d93950d2dce1408470ed516179c
Jordan Landry <jordanlandry1999@gmail.com>
2022-10-25 11:38:44 -0400

    Added more notes + helpful functions
    + Added a function to get the note value and octave by the guitar string and fret

--- src/components/TabPage.tsx
+++ src/components/TabPage.tsx
@@ -3,12 +3,12 @@ import { PlayFill } from "react-bootstrap-icons";
 import { test } from "../data/test";
 import sound from "../assets/audio/test.wav";
 
-import g2 from "../assets/audio/g2.wav";
-import g3 from "../assets/audio/g3.wav";
-import g4 from "../assets/audio/g4.wav";
-import b3 from "../assets/audio/b3.wav";
-import d2 from "../assets/audio/d3.wav";
-import b2 from "../assets/audio/b2.wav";
+// import g2 from "../assets/audio/g2.wav";
+// import g3 from "../assets/audio/g3.wav";
+// import g4 from "../assets/audio/g4.wav";
+// import b3 from "../assets/audio/b3.wav";
+// import d2 from "../assets/audio/d3.wav";
+// import b2 from "../assets/audio/b2.wav";
 
 import Chart from "./Chart";
 import { noteData } from "../data/interfaces";
@@ -21,7 +21,47 @@ export default function TabPage() {
   const a = test;
 
   const play = (a: any) => {
+    const audio = new Audio();
+    audio.src = a;
+
     new Audio(a).play();
+  };
+
+  const tuning = ["e2", "a3", "d3", "g3", "b4", "e4"];
+  // const tuning = 'a';
+
+  const notes = [
+    "ab",
+    "a",
+    "bb",
+    "b",
+    "c",
+    "db",
+    "d",
+    "eb",
+    "e",
+    "f",
+    "gb",
+    "g",
+  ];
+  const getNote = (fret: number, guitarString: number): string => {
+    // Get notes index of initial string note, add the stringNumber value,
+    // and get the remainder of it and the number of notes to get the note index,
+    // floor divide it to get the octave
+
+    let n = tuning[guitarString - 1][0];
+    // let o = tuning[guitarString - 1][1];
+
+    let o = "";
+    if (n.length === 3) {
+      n += tuning[guitarString - 1][1];
+      o = tuning[guitarString - 1][2];
+    } else o = tuning[guitarString - 1][1];
+
+    let i = notes.indexOf(n) + fret;
+    o = parseInt(o) + Math.floor(i / notes.length) + ""; // Get octave
+
+    return notes[i % notes.length] + o;
   };
 
   let prevBeat = 0;
@@ -32,15 +72,18 @@ export default function TabPage() {
         // Run through the beats
         for (let i = prevBeat; i < note.beatCount; i++) {
           if (i === note.beatCount - 1) {
-            if (note.guitarString === 1 && note.fret === 3) play(g2);
-            if (note.guitarString === 2 && note.fret === 2) play(b2);
-            if (note.guitarString === 3 && note.fret === 0) play(d2);
-            if (note.guitarString === 4 && note.fret === 0) play(g3);
-            if (note.guitarString === 5 && note.fret === 0) play(b3);
-            if (note.guitarString === 6 && note.fret === 3) play(g4);
+            let n = getNote(note.fret, note.guitarString);
+            play("src/assets/audio/" + n + ".wav");
+
+            // if (note.guitarString === 1 && note.fret === 3) play(g2);
+            // if (note.guitarString === 2 && note.fret === 2) play(b2);
+            // if (note.guitarString === 3 && note.fret === 0) play(d2);
+            // if (note.guitarString === 4 && note.fret === 0) play(g3);
+            // if (note.guitarString === 5 && note.fret === 0) play(b3);
+            // if (note.guitarString === 6 && note.fret === 3) play(g4);
           }
           setOffset((prev) => offset + 5);
-          await sleep(750);
+          await sleep(200);
         }
       }
     });
--- src/data/test.ts
+++ src/data/test.ts
@@ -1,58 +1,129 @@
 import { songType } from "./interfaces";
 
 export const test: songType = {
-  name: "G Chord",
+  name: "Under a glass Moon",
   bpm: 100,
 
   // Should play a G chord
+
+  // This isn't how you will write tabs, this is just how it will be stored and read
   data: [
-    { guitarString: 6, fret: 3, beatCount: 1, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 1, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 1, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 1, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 1, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 1, type: "normal" },
+    { guitarString: 6, fret: 12, beatCount: 1, type: "normal" },
+    { guitarString: 5, fret: 14, beatCount: 1, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 2, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 2, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 2, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 2, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 2, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 2, type: "normal" },
+    { guitarString: 5, fret: 14, beatCount: 2, type: "normal" },
+    { guitarString: 5, fret: 14, beatCount: 3, type: "normal" },
+    { guitarString: 5, fret: 14, beatCount: 4, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 3, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 3, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 3, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 3, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 3, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 3, type: "normal" },
+    { guitarString: 5, fret: 12, beatCount: 5, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 5, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 5, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 5, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 5, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 5, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 5, type: "normal" },
+    { guitarString: 5, fret: 12, beatCount: 6, type: "normal" },
+    { guitarString: 5, fret: 12, beatCount: 7, type: "normal" },
+    { guitarString: 5, fret: 12, beatCount: 8, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 6, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 7, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 8, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 9, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 10, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 11, type: "normal" },
+    { guitarString: 5, fret: 7, beatCount: 9, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 17, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 16, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 15, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 14, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 13, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 12, type: "normal" },
+    { guitarString: 5, fret: 7, beatCount: 10, type: "normal" },
+    { guitarString: 5, fret: 7, beatCount: 11, type: "normal" },
+    { guitarString: 5, fret: 7, beatCount: 12, type: "normal" },
 
-    { guitarString: 6, fret: 3, beatCount: 18, type: "normal" },
-    { guitarString: 5, fret: 0, beatCount: 18, type: "normal" },
-    { guitarString: 4, fret: 0, beatCount: 18, type: "normal" },
-    { guitarString: 3, fret: 0, beatCount: 18, type: "normal" },
-    { guitarString: 2, fret: 2, beatCount: 18, type: "normal" },
-    { guitarString: 1, fret: 3, beatCount: 18, type: "normal" },
+    { guitarString: 5, fret: 9, beatCount: 13, type: "normal" },
+
+    { guitarString: 5, fret: 9, beatCount: 14, type: "normal" },
+    { guitarString: 5, fret: 9, beatCount: 15, type: "normal" },
+    { guitarString: 5, fret: 9, beatCount: 16, type: "normal" },
+
+    { guitarString: 5, fret: 5, beatCount: 17, type: "normal" },
+
+    { guitarString: 5, fret: 5, beatCount: 18, type: "normal" },
+    { guitarString: 5, fret: 5, beatCount: 19, type: "normal" },
+
+    { guitarString: 5, fret: 4, beatCount: 20, type: "normal" },
+    { guitarString: 5, fret: 5, beatCount: 21, type: "normal" },
+    { guitarString: 5, fret: 4, beatCount: 22, type: "normal" },
+    { guitarString: 4, fret: 6, beatCount: 23, type: "normal" },
+    { guitarString: 4, fret: 4, beatCount: 24, type: "normal" },
+    { guitarString: 4, fret: 8, beatCount: 25, type: "normal" },
+    { guitarString: 4, fret: 4, beatCount: 26, type: "normal" },
+
+    { guitarString: 3, fret: 6, beatCount: 27, type: "normal" },
+    { guitarString: 3, fret: 4, beatCount: 28, type: "normal" },
+    { guitarString: 2, fret: 6, beatCount: 29, type: "normal" },
+
+    { guitarString: 5, fret: 16, beatCount: 32, type: "normal" },
+    { guitarString: 6, fret: 12, beatCount: 33, type: "normal" },
+
+    { guitarString: 6, fret: 16, beatCount: 34, type: "normal" },
+    { guitarString: 6, fret: 12, beatCount: 35, type: "normal" },
+    { guitarString: 5, fret: 16, beatCount: 36, type: "normal" },
+    { guitarString: 5, fret: 12, beatCount: 37, type: "normal" },
+
+    { guitarString: 4, fret: 13, beatCount: 38, type: "normal" },
+    { guitarString: 4, fret: 9, beatCount: 39, type: "normal" },
+    { guitarString: 3, fret: 13, beatCount: 40, type: "normal" },
+    { guitarString: 3, fret: 9, beatCount: 41, type: "normal" },
+
+    { guitarString: 2, fret: 11, beatCount: 42, type: "normal" },
+    { guitarString: 2, fret: 7, beatCount: 43, type: "normal" },
+    { guitarString: 1, fret: 11, beatCount: 44, type: "normal" },
+    { guitarString: 1, fret: 7, beatCount: 45, type: "normal" },
+
+    { guitarString: 1, fret: 0, beatCount: 46, type: "normal" },
+    { guitarString: 1, fret: 12, beatCount: 48, type: "normal" },
+    { guitarString: 1, fret: 4, beatCount: 50, type: "normal" },
+    { guitarString: 1, fret: 7, beatCount: 52, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 1, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 1, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 1, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 1, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 1, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 1, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 2, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 2, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 2, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 2, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 2, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 2, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 3, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 3, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 3, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 3, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 3, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 3, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 5, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 5, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 5, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 5, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 5, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 5, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 6, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 7, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 8, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 9, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 10, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 11, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 17, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 16, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 15, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 14, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 13, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 12, type: "normal" },
+
+    // { guitarString: 6, fret: 3, beatCount: 18, type: "normal" },
+    // { guitarString: 5, fret: 0, beatCount: 18, type: "normal" },
+    // { guitarString: 4, fret: 0, beatCount: 18, type: "normal" },
+    // { guitarString: 3, fret: 0, beatCount: 18, type: "normal" },
+    // { guitarString: 2, fret: 2, beatCount: 18, type: "normal" },
+    // { guitarString: 1, fret: 3, beatCount: 18, type: "normal" },
+
+    // { guitarString: 1, fret: 0, beatCount: 20, type: "normal" },
+    // { guitarString: 1, fret: 0, beatCount: 21, type: "normal" },
+    // { guitarString: 1, fret: 0, beatCount: 22, type: "normal" },
   ],
 };
